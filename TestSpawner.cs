d1fb5d87fa74da955fa7cd356651a26dffcbbada
bah333 <37232348+bah333@users.noreply.github.com>
2020-03-03 12:35:29 -0700

    Add files via upload
    A sample scene of 2 spawners sending asteroids to collide with each other. The Asteroid prefab and ProceduralAsteroid.cs are all you need for the asteroids. The only code that will need to be added is code inside the OnCollisionEnter function to ensure the asteroid interacts with other objects such as a space ship collision or collision with weapon projectiles properly. I've placed OnCollisionEnter as the first function within the class. Asteroids will collide and act with proper physics any object with a collider and Rigidbody attached. Some public variables will need to be given a value in the inspector or whatever script is instantiating an asteroid and some don't, read the comments attached to public variable declarations in script for clarification. Feel free to read the code for instantiating an asteroid at the bottom of ProceduralAsteroid.cs or TestSpawner.cs for an example of how the need to be Instantiated.

---
+++ TestSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawner : MonoBehaviour
+{
+
+    public GameObject asteroid;
+    public float initialDelay = 0.0f;
+    public float delay = 2.5f;
+    public float asteroidLifeTime = 4.0f;
+    public Vector3 worldSpaceVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("instantiateAsteroid", initialDelay, delay);
+    }
+
+    void instantiateAsteroid()
+    {
+        //int randIndex = Random.Range(0, projectiles.Length);
+        asteroid.GetComponent<ProceduralAsteroid>().materialType = Random.Range(1, 4);
+        asteroid.GetComponent<ProceduralAsteroid>().sphereRadius = Random.Range(0.5f, 3.1f);
+        asteroid.GetComponent<ProceduralAsteroid>().initialForwardVelocity = worldSpaceVelocity;
+        
+        GameObject newInstance = Instantiate(asteroid, this.gameObject.transform);
+        //Destroy(newInstance, asteroidLifeTime);
+        //Rigidbody rb = newInstance.GetComponent<Rigidbody>();
+        //rb.velocity = new Vector3(0, 0, -2);
+    }
+}
