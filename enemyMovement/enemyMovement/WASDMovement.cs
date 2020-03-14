b5122d25a9b00b93277041f36420f823507e1d26
hoffmalu <48805841+hoffmalu@users.noreply.github.com>
2020-03-13 19:14:36 -0600

    updated enemy movement
    Still lots of spaghetti code
    -Enemies can spawn in a random location within a radius of the player
    -Enemies can avoid obstacles, and are destroyed if they end up colliding with another object

---
+++ enemyMovement/enemyMovement/WASDMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDMovement : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float maxSpeed = 10.0f;
+    public float turnSpeed = 3.0f;
+    public Rigidbody rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * speed);
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -turnSpeed, 0);
+            // rb.AddTorque(transform.up * turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, turnSpeed, 0);
+            //  rb.AddTorque(-transform.up * turnSpeed);
+        }
+
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(2.0f, 0, 0);
+        }
+
+           if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(-2.0f, 0, 0);
+        }
+        
+        Vector3 slowDown = rb.velocity * -1;
+        rb.AddForce(slowDown * .01f, ForceMode.Impulse);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+    }
+}
